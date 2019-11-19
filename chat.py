8fc7979a51cac6d91ea59431fb83f71b98da837b
lucas <lucas-antonio2011@hotmail.com>
2019-11-19 19:16:30 -0300

    mudanças feitas no bot, para que fale sobre filmes

--- chat.py
+++ chat.py
@@ -1,24 +1,29 @@
 from chatterbot.trainers import ListTrainer
 from chatterbot import ChatBot
 
-bot = ChatBot('TW Chat Bot')
+bot = ChatBot('Baymax')
 
-conversa = ['oi', 'olá', 'olá, bom dia', 'bom dia', 'bom dia,como vai?', 'estou bem']
-
-conversa2 = ['qual seu filme favorito?','eu gosto de batman', 'meu filme favorito é o homem de ferro','Você gosta do tony stark?', 'Você gosta de cinema?','cinema é muito legal']
+conversa = ['oi','ola'
+'qual seu filme favorito?','homem de ferro'
+'qual seu personagem favorito?','Batman'
+'me recomenda um filme de ação','velozes e furiosos'
+'qual seu personagem favorito da vida real?','theRock'
+'você gosta de filme de terro?','prefiro filme de aventura'
+'qual seu estilo de filme ?','prefiro ação e aventura'
+'quem é o theRock?', 'um ator renomado dos cinemas'
+'cinema é seu lugar favorito?','claro,filmes são muit bons']
 
 trainer = ListTrainer (bot)
 
 trainer.train (conversa)
-trainer.train(conversa2)
 
 while True:
     pergunta = input("Você: ")
 
     resposta = bot.get_response(pergunta)
       
-    if float(resposta.confidence) > 0.5:
-        print('TW Bot: ', resposta)
+    if float(resposta.confidence) > 0.3:
+        print('Baymax: ', resposta)
     else:
-        print('TW Bot: Não entendi')
+        print('Baymax: Não entendi')
    
